9e600534431fc398afd9a45511c660d8f74b173a
jieun <23608596+jieunyoo@users.noreply.github.com>
2022-02-04 09:37:26 -0500

    add graphs

--- doge.py
+++ doge.py
@@ -6,7 +6,7 @@ import re
 import base64
 import altair as alt
 
-st.title('Doge Coin Volume')
+st.title('Doge Coin Stats!')
 
 st.text('Use your mouse to interact with this graph!')
 st.text('Data from Nov. 11 2017 to Jan. 28 2022')
@@ -26,5 +26,14 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
+st.subheader('Volume')
 chart = alt.Chart(data).mark_circle().encode( x='Date:T', y = 'Volume').interactive()
 st.altair_chart(chart, use_container_width=True)
+
+st.subheader('Open Price')
+chartOpenValue = alt.Chart(data).mark_circle().encode( x='Date:T', y = 'Open').interactive()
+st.altair_chart(chartOpenValue, use_container_width=True)
+
+st.subheader('Close Price')
+chartCloseValue = alt.Chart(data).mark_circle().encode( x='Date:T', y = 'Close').interactive()
+st.altair_chart(chartCloseValue, use_container_width=True)
